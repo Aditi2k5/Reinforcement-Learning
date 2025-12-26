d3f848b425bbc0308b270cfd71d7d8b04e9f2d20
Aditi P <adip.2k5@gmail.com>
2025-12-26 12:22:49 +0530

    dqn done

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -95,68 +95,165 @@ class FrozenLakeDQL():
                         action = policy_dqn(self.state_to_dqn_input(state, num_states)).argmax().item()
                 
                 #exec action
-                new_state,reward,terminared
+                new_state,reward,terminated, truncared, _=env.step(action)
 
+                #save exp in memory
+                memory.append((state, action, new_state, reward, terminated))
 
+                #next state
+                state=new_state
 
-def run(episodes, is_training=True, render=False):
-    env=gym.make('FrozenLake-v1', map_name="8x8", is_slippery=True, render_mode='human' if render else None)
-    
-    if (is_training):
-        q=np.zeros((env.observation_space.n, env.action_space.n)) #init the array
-    else:
-        f=open('frozen_lakes.pkl','rb')
-        q=pickle.load(f)
-        f.close()
+                #increment step count
+                step+=1
 
+            #keep track of rewards collected
+            if reward==1:
+                rewards_per_ep[i]=1
 
-    learning_rate_a=0.9 #alpha for q learning
-    discount_factor_g=0.9 #gamma
+            #check if enough exp has been collected and atleast 1 reward got
+            if len(memory)>self.min_batch_size and np.sum(rewards_per_ep)>0:
+                min_batch=memory.sample(self.min_batch_size)
+                self.optimize(min_batch, policy_dqn,target_dqn)
 
-    #epsilon greedy approach
-    epsilon=1 #1=fully random actions, ovvertime can decrease randomness
-    epsilon_decay_rate=0.0001 #decay rate, direct impact on no of eps needed to train
-    # for it to go till zero, train for 10k eps as 1/0.0001
-    rng=np.random.default_rng() #rando num generator
-    rewards_per_episode = np.zeros(episodes)
+                #decay the epsilon
+                epsilon=max(epsilon-1/episodes,0)
+                epsilon_history.append(epsilon)
 
-    for i in range(episodes):
-        state=env.reset()[0]
-        terminated=False
-        truncated=False
+                #copy pol to target after some number of steps
+                if step>self.newtork_sync_rate:
+                    target_dqn.load_state_dict(policy_dqn.state_dict())
+                    step=0
+        #close env
+        env.close()
 
-        while(not terminated and not truncated):
-            if is_training and rng.random()<epsilon:
-                action = env.action_space.sample() #take random actions, 0=left, 1=down, 2=right, 3=up
+        #save pol
+        torch.save(policy_dqn.state_dict(), 'frozenlake.pt')
+
+        #create new graph
+        plt.figure(1)
+
+         # Plot average rewards (Y-axis) vs episodes (X-axis)
+        sum_rewards = np.zeros(episodes)
+        for x in range(episodes):
+            sum_rewards[x] = np.sum(rewards_per_episode[max(0, x-100):(x+1)])
+        plt.subplot(121) # plot on a 1 row x 2 col grid, at cell 1
+        plt.plot(sum_rewards)
+        
+        # Plot epsilon decay (Y-axis) vs episodes (X-axis)
+        plt.subplot(122) # plot on a 1 row x 2 col grid, at cell 2
+        plt.plot(epsilon_history)
+        
+        # Save plots
+        plt.savefig('frozen_lake_dql.png')
+
+    # Optimize policy network
+    def optimize(self, mini_batch, policy_dqn, target_dqn):
+
+        # Get number of input nodes
+        num_states = policy_dqn.fc1.in_features
+
+        current_q_list = []
+        target_q_list = []
+
+        for state, action, new_state, reward, terminated in mini_batch:
+
+            if terminated: 
+                # Agent either reached goal (reward=1) or fell into hole (reward=0)
+                # When in a terminated state, target q value should be set to the reward.
+                target = torch.FloatTensor([reward])
             else:
-                action=np.argmax(q[state,:]) # follow q table
-            new_state, reward, terminated, truncated,_=env.step(action)
+                # Calculate target q value 
+                with torch.no_grad():
+                    target = torch.FloatTensor(
+                        reward + self.discount_factor_g * target_dqn(self.state_to_dqn_input(new_state, num_states)).max()
+                    )
 
-            if is_training:
-                q[state, action]=q[state, action] + learning_rate_a *(
-                    reward+discount_factor_g * np.max(q[new_state,:])-q[state,action])
+            # Get the current set of Q values
+            current_q = policy_dqn(self.state_to_dqn_input(state, num_states))
+            current_q_list.append(current_q)
 
-            state=new_state
-    
-        epsilon = max(epsilon-epsilon_decay_rate, 0) #after each episode decrease epsilon all the way till zero
+            # Get the target set of Q values
+            target_q = target_dqn(self.state_to_dqn_input(state, num_states)) 
+            # Adjust the specific action to the target that was just calculated
+            target_q[action] = target
+            target_q_list.append(target_q)
+                
+        # Compute loss for the whole minibatch
+        loss = self.loss_fn(torch.stack(current_q_list), torch.stack(target_q_list))
 
-        if (epsilon==0):
-            learning_rate_a=0.0001 #reduce lr to help stabilize q values once we r not exploring much
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
-        if reward==1:
-            rewards_per_episode[i]=1
-    env.close()
+    '''
+    Converts an state (int) to a tensor representation.
+    For example, the FrozenLake 4x4 map has 4x4=16 states numbered from 0 to 15. 
 
-    sum_rewards = np.zeros(episodes)
-    for j in range(episodes):
-        sum_rewards[j] = np.sum(rewards_per_episode[max(0,j-100):(j+1)])
-    plt.plot(sum_rewards)
-    plt.savefig('frozen_lakes_new.png')
+    Parameters: state=1, num_states=16
+    Return: tensor([0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
+    '''
+    def state_to_dqn_input(self, state:int, num_states:int)->torch.Tensor:
+        input_tensor = torch.zeros(num_states)
+        input_tensor[state] = 1
+        return input_tensor
 
-    if is_training:
-        f=open('frozen_lakes.pkl','wb')
-        pickle.dump(q,f)
-        f.close()
+    # Run the FrozeLake environment with the learned policy
+    def test(self, episodes, is_slippery=False):
+        # Create FrozenLake instance
+        env = gym.make('FrozenLake-v1', map_name="4x4", is_slippery=is_slippery, render_mode='human')
+        num_states = env.observation_space.n
+        num_actions = env.action_space.n
+
+        # Load learned policy
+        policy_dqn = DQN(in_states=num_states, h1_nodes=num_states, out_actions=num_actions) 
+        policy_dqn.load_state_dict(torch.load("frozen_lake_dql.pt"))
+        policy_dqn.eval()    # switch model to evaluation mode
+
+        print('Policy (trained):')
+        self.print_dqn(policy_dqn)
+
+        for i in range(episodes):
+            state = env.reset()[0]  # Initialize to state 0
+            terminated = False      # True when agent falls in hole or reached goal
+            truncated = False       # True when agent takes more than 200 actions            
+
+            # Agent navigates map until it falls into a hole (terminated), reaches goal (terminated), or has taken 200 actions (truncated).
+            while(not terminated and not truncated):  
+                # Select best action   
+                with torch.no_grad():
+                    action = policy_dqn(self.state_to_dqn_input(state, num_states)).argmax().item()
+
+                # Execute action
+                state,reward,terminated,truncated,_ = env.step(action)
+
+        env.close()
+
+    # Print DQN: state, best action, q values
+    def print_dqn(self, dqn):
+        # Get number of input nodes
+        num_states = dqn.fc1.in_features
+
+        # Loop each state and print policy to console
+        for s in range(num_states):
+            #  Format q values for printing
+            q_values = ''
+            for q in dqn(self.state_to_dqn_input(s, num_states)).tolist():
+                q_values += "{:+.2f}".format(q)+' '  # Concatenate q values, format to 2 decimals
+            q_values=q_values.rstrip()              # Remove space at the end
+
+            # Map the best action to L D R U
+            best_action = self.ACTIONS[dqn(self.state_to_dqn_input(s, num_states)).argmax()]
+
+            # Print policy in the format of: state, action, q values
+            # The printed layout matches the FrozenLake map.
+            print(f'{s:02},{best_action},[{q_values}]', end=' ')         
+            if (s+1)%4==0:
+                print() # Print a newline every 4 states
 
 if __name__ == '__main__':
-    run(1000, is_training=False, render=False)
+
+    frozen_lake = FrozenLakeDQL()
+    is_slippery = False
+    frozen_lake.train(1000, is_slippery=is_slippery)
+    frozen_lake.test(10, is_slippery=is_slippery)
